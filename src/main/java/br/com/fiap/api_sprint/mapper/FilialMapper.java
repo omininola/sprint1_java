f8403087ee03fcea09ac38aea3a468fcf78cc168
Otavio Miklos <otaviomiklos@gmail.com>
2025-05-17 19:18:28 -0300

    chore: adding id to response

--- src/main/java/br/com/fiap/api_sprint/mapper/FilialMapper.java
+++ src/main/java/br/com/fiap/api_sprint/mapper/FilialMapper.java
@@ -7,35 +7,36 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
-import br.com.fiap.api_sprint.dto.filial.FilialRequestDTO;
-import br.com.fiap.api_sprint.dto.filial.FilialResponseDTO;
+import br.com.fiap.api_sprint.dto.filial.FilialRequest;
+import br.com.fiap.api_sprint.dto.filial.FilialResponse;
 import br.com.fiap.api_sprint.entity.Filial;
 
 public class FilialMapper {
 
-  public static Filial requestToFilial(FilialRequestDTO filialRequestDTO) {
+  public static Filial requestToFilial(FilialRequest filialRequest) {
     Filial filial = new Filial();
-    filial.setNome(filialRequestDTO.getNome());
-    filial.setEndereco(filialRequestDTO.getEndereco());
+    filial.setNome(filialRequest.getNome());
+    filial.setEndereco(filialRequest.getEndereco());
     return filial;
   }
 
-  public static FilialResponseDTO filialToResponse(Filial filial) {
-    FilialResponseDTO filialResponseDTO = new FilialResponseDTO();
-    filialResponseDTO.setNome(filial.getNome());
-    filialResponseDTO.setEndereco(filial.getEndereco());
-    filialResponseDTO.setAreas(filial.getAreas());
-    return filialResponseDTO;
+  public static FilialResponse filialToResponse(Filial filial) {
+    FilialResponse filialResponse = new FilialResponse();
+    filialResponse.setId(filial.getId());
+    filialResponse.setNome(filial.getNome());
+    filialResponse.setEndereco(filial.getEndereco());
+    filialResponse.setAreas(filial.getAreas());
+    return filialResponse;
   }
 
-  public static List<FilialResponseDTO> filialToResponse(List<Filial> filiais) {
-    List<FilialResponseDTO> filiaisResponseDTO = filiais.stream().map(FilialMapper::filialToResponse).toList();
+  public static List<FilialResponse> filialToResponse(List<Filial> filiais) {
+    List<FilialResponse> filiaisResponseDTO = filiais.stream().map(FilialMapper::filialToResponse).toList();
     return filiaisResponseDTO;
   }
 
-  public static Page<FilialResponseDTO> filiaisToPage(List<Filial> filiais) {
+  public static Page<FilialResponse> filiaisToPage(List<Filial> filiais) {
     Pageable defaultPage = PageRequest.of(0, 10);
-    Page<FilialResponseDTO> page = new PageImpl<FilialResponseDTO>(FilialMapper.filialToResponse(filiais), defaultPage,
+    Page<FilialResponse> page = new PageImpl<FilialResponse>(FilialMapper.filialToResponse(filiais), defaultPage,
         10);
     return page;
   }
